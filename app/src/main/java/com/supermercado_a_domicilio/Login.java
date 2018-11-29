f97b8f94c90c3b9e0f21facc478dbe208cf49cf0
aledavila98 <aled98.ad@gmail.com>
2018-11-29 16:19:02 -0600

    para hacer switch

--- app/src/main/java/com/supermercado_a_domicilio/Login.java
+++ app/src/main/java/com/supermercado_a_domicilio/Login.java
@@ -1,13 +1,87 @@
 package com.supermercado_a_domicilio;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.ActionCodeSettings;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
-    @Override
+    private static final String TAG = "Logjn";
+    Button bCreate, bLogin;
+    EditText etEmail;
+    ActionCodeSettings actionCodeSettings;
+
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+        buildActionCodeSettings();
+
+        bCreate = (Button)findViewById(R.id.bCrear);
+        bLogin = (Button)findViewById(R.id.bLogin);
+        etEmail = (EditText)findViewById(R.id.etEmail);
+
+        bCreate.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String email = etEmail.getText().toString();
+                        sendSignInLink(email,actionCodeSettings);
+                    }
+                }
+        );
+
     }
+
+    public void buildActionCodeSettings() {
+        actionCodeSettings =
+                ActionCodeSettings.newBuilder()
+                        // URL you want to redirect back to. The domain (www.example.com) for this
+                        // URL must be whitelisted in the Firebase Console.
+                        .setUrl("servicio-a-domicilio-5c125.firebaseapp.com")
+                        // This must be true
+                        .setHandleCodeInApp(true)
+                        .setIOSBundleId("com.example.ios")
+                        .setAndroidPackageName(
+                                "com.supermercado_a_domicilio",
+                                true, /* installIfNotAvailable */
+                                "16"    /* minimumVersion */)
+                        .build();
+    }
+
+    public void sendSignInLink(String email, ActionCodeSettings actionCodeSettings) {
+        // [START auth_send_sign_in_link]
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.sendSignInLinkToEmail(email, actionCodeSettings)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Email sent.");
+                        }
+                    }
+                });
+        // [END auth_send_sign_in_link]
+    }
+
+    public void checkCurrentUser() {
+        // [START check_current_user]
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // User is signed in
+        } else {
+            // No user is signed in
+        }
+        // [END check_current_user]
+    }
+
 }
--- app/src/main/java/com/supermercado_a_domicilio/MainActivity.java
+++ app/src/main/java/com/supermercado_a_domicilio/MainActivity.java
@@ -1,13 +1,27 @@
 package com.supermercado_a_domicilio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //Intent intent = new Intent(this, Login.class);
+
+                    }
+                }
+        );
     }
 }
